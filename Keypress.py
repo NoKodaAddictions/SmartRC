6ac8b2f67fb950dba489b7c07c4191472d45af07
NoKoda <airomech404@gmail.com>
2021-02-21 13:19:59 -0500

    Alpha 0.1.5 Update
    UI Change

--- Keypress.py
+++ Keypress.py
@@ -3,12 +3,12 @@ import socket
 import sys
 import datetime
 import time
+import os
 
 def on_press(key):
     keypress = "{0}".format(key)
     control.sendall(bytes(keypress,"utf-8"))
     result = control.recv(1024).decode("utf-8")
-    print(result)
 
     fh = open(f"Logs\Commands-{datetime.date.today()}.csv",'a')
     current = time.strftime("%H:%M:%S", time.localtime())
@@ -16,8 +16,19 @@ def on_press(key):
     fh.close()
 
 def Control():
-    fh = open(f"Logs\Flight-{datetime.date.today()}.csv",'w')
-    fh.close()
+    flightnum = 0
+    if os.path.exists(f"Logs\Commands-{datetime.date.today()}.csv") == True:
+        flightnum += 1
+        while True:
+            if os.path.exists(f"Logs\Commands-{datetime.date.today()}-{flightnum}.csv") == True:
+                flightnum +=1
+                continue
+            else:
+                fh.open(f"Logs\Commands-{datetime.date.today()}-{flightnum}.csv",'w')
+                fh.close()
+                break
+
+
     global control
     control = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -27,24 +38,29 @@ def Control():
 
     with Listener(on_press=on_press) as listener:
         listener.join()
-        print(listener.join())
+    
 
-def Info():
+def Info(Throttle, Airspeed, Altitude, Roll, Pitch, Yaw):
+    flightnum = 0
+    if os.path.exists(f"Logs\Flight-{datetime.date.today()}.csv") == True:
+        flightnum += 1
+        while True:
+            if os.path.exists(f"Logs\Flight-{datetime.date.today()}-{flightnum}.csv") == True:
+                flightnum +=1
+                continue
+            else:
+                break
     fh = open(f"Logs\Flight-{datetime.date.today()}.csv",'w')
     fh.close()
     info = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         info.connect(("", 64220))
     except:
-        Info()
-    try:
-        while True:
-            recv = info.recv(1024)
-            print(recv.decode())
-            current = time.strftime("%H:%M:%S", time.localtime())
-            fh = open(f"Logs\Flight-{datetime.date.today()}.csv", 'a')
-            fh.write(f'{recv.decode("utf-8")},{current}\n')
-            fh.close()
-    except KeyboardInterrupt:
+        Info(Throttle, Airspeed, Altitude, Roll, Pitch, Yaw)        
+    while True:
+        recv = info.recv(1024)
+        stat = recv.decode().split(",")
+        current = time.strftime("%H:%M:%S", time.localtime())
+        fh = open(f"Logs\Flight-{datetime.date.today()}.csv", 'a')
+        fh.write(f'{recv.decode("utf-8")},{current}\n')
         fh.close()
-        sys.exit()
